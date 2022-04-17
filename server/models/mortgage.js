ca7b5887475b5ef65dd042311ac07d8d646046da
Sagi <sagi403@gmail.com>
2022-04-17 14:56:19 +0300

    Defining the mortgage model

---
+++ server/models/mortgage.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const mortgageSchema = new mongoose.Schema({
+  mortgageAmount: {
+    type: Number,
+    required: true,
+  },
+  termYearly: {
+    type: Number,
+    required: true,
+  },
+  interestYearly: {
+    type: Number,
+    required: true,
+  },
+  interestMonthly: {
+    type: Number,
+  },
+  monthlyPayment: {
+    type: Number,
+  },
+  totalCost: {
+    type: Number,
+  },
+  totalInterest: {
+    type: Number,
+  },
+});
+
+const Mortgage = mongoose.model("Mortgage", mortgageSchema);
+
+export { Mortgage };
