de362a32a7787f3f749320ccb1f7aa7f684ce3fd
Sagi <sagi403@gmail.com>
2022-04-18 14:44:09 +0300

    Connecting to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import { newMortgageRouter } from "./routes/newMortgage.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectDB();
 
 const app = express();
 app.use(bodyParser.json());
