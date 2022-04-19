e7bf9d716aed1593f2c41e3638f861f37055ac61
Sagi <sagi403@gmail.com>
2022-04-19 15:33:36 +0300

    Testing mortgage creation and calculation

--- server/src/routes/__test__/newMortgage.test.js
+++ server/src/routes/__test__/newMortgage.test.js
@@ -11,3 +11,25 @@ it("has a route handler listening to /api/total-cost for post requests", async (
 
   expect(response.status).not.toEqual(404);
 });
+
+it("creates a mortgage with valid parameters", async () => {
+  let mortgage = await Mortgage.find({});
+  expect(mortgage.length).toEqual(0);
+
+  await request(app)
+    .post("/api/total-cost")
+    .send({
+      mortgageAmount: 500000,
+      termYearly: 20,
+      interestYearly: 3.5,
+    })
+    .expect(201);
+
+  mortgage = await Mortgage.find({});
+
+  expect(mortgage.length).toEqual(1);
+  expect(mortgage[0].interestMonthly.toFixed(2)).toEqual("0.29");
+  expect(mortgage[0].monthlyPayment.toFixed(2)).toEqual("2885.71");
+  expect(mortgage[0].totalCost.toFixed(2)).toEqual("692569.80");
+  expect(mortgage[0].totalInterest.toFixed(2)).toEqual("192569.80");
+});
