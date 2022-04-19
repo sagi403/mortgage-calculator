50454f68803b9b2e2fdf5aed0d9af78f422600be
Sagi <sagi403@gmail.com>
2022-04-19 15:32:00 +0300

    Testing route handler listening

--- server/src/routes/__test__/newMortgage.test.js
+++ server/src/routes/__test__/newMortgage.test.js
@@ -3,6 +3,11 @@ import { app } from "../../app.js";
 import { Mortgage } from "../../models/mortgage.js";
 
 it("has a route handler listening to /api/total-cost for post requests", async () => {
-  // const response = await request(app).post("/api/total-cost").send({});
-  // expect(response.status).not.toEqual(404);
+  const response = await request(app).post("/api/total-cost").send({
+    mortgageAmount: 500000,
+    termYearly: 20,
+    interestYearly: 3.5,
+  });
+
+  expect(response.status).not.toEqual(404);
 });
