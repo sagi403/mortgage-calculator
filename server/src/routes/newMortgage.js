36e47d7d3d0169c30259e36dbb32ab89cd2fd6c3
Sagi <sagi403@gmail.com>
2022-04-19 16:06:21 +0300

    Adding validation to route

--- server/src/routes/newMortgage.js
+++ server/src/routes/newMortgage.js
@@ -1,27 +1,52 @@
 import express from "express";
 import { body } from "express-validator";
 import totalMortgage from "../calculations/total-mortgage.js";
+import { validateRequest } from "../middleware/validate-request.js";
 import { Mortgage } from "../models/mortgage.js";
 
 const router = express.Router();
 
-router.post("/api/total-cost", async (req, res) => {
-  const { mortgageAmount, termYearly, interestYearly } = req.body;
-  const { interestMonthly, monthlyPayment, totalCost, totalInterest } =
-    totalMortgage(mortgageAmount, termYearly, interestYearly);
+router.post(
+  "/api/total-cost",
+  [
+    body("mortgageAmount")
+      .not()
+      .isEmpty()
+      .withMessage("Mortgage amount is required"),
+    body("termYearly").not().isEmpty().withMessage("Yearly term is required"),
+    body("interestYearly")
+      .not()
+      .isEmpty()
+      .withMessage("Yearly interest rate is required"),
+    body("mortgageAmount")
+      .isFloat({ gt: 0 })
+      .withMessage("Mortgage amount must be greater than 0"),
+    body("termYearly")
+      .isFloat({ gt: 0 })
+      .withMessage("Yearly term must be greater than 0"),
+    body("interestYearly")
+      .isNumeric()
+      .withMessage("Yearly interest rate must be a number"),
+  ],
+  validateRequest,
+  async (req, res) => {
+    const { mortgageAmount, termYearly, interestYearly } = req.body;
+    const { interestMonthly, monthlyPayment, totalCost, totalInterest } =
+      totalMortgage(mortgageAmount, termYearly, interestYearly);
 
-  const mortgage = new Mortgage({
-    mortgageAmount,
-    termYearly,
-    interestYearly,
-    interestMonthly,
-    monthlyPayment,
-    totalCost,
-    totalInterest,
-  });
-  await mortgage.save();
+    const mortgage = new Mortgage({
+      mortgageAmount,
+      termYearly,
+      interestYearly,
+      interestMonthly,
+      monthlyPayment,
+      totalCost,
+      totalInterest,
+    });
+    await mortgage.save();
 
-  res.status(201).send(mortgage);
-});
+    res.status(201).send(mortgage);
+  }
+);
 
 export { router as newMortgageRouter };
