fc9c10bc6758e120749a3345946eb5c4660217df
Sagi <sagi403@gmail.com>
2022-04-23 13:53:24 +0300

    Fixing error display issue

--- server/src/errors/request-validation-error.js
+++ server/src/errors/request-validation-error.js
@@ -3,6 +3,7 @@ export class RequestValidationError extends Error {
 
   constructor(errors) {
     super();
+    this.errors = errors;
 
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
